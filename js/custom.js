81fa9ba471fceac4437874b82168729f113d7057
JAZZAR <hussaine70@gmail.com>
2026-06-08 09:44:13 +0300

    Finish Responsive Design

--- js/custom.js
+++ js/custom.js
@@ -3,5 +3,7 @@ $(function () {
   "use strict";
   $(".info-list li").click(function () {
     $(this).addClass("sellected").siblings("li").removeClass("sellected");
+    $(".info-content div").hide();
+    $("." + $(this).data("class")).fadeIn();
   });
 });
